c8eeb86a2239b8f253efa11a65c348d1345fb9e3
Shingo Kawamura <blp1526@gmail.com>
2017-02-28 21:19:38 +0900

    Add user variable

--- provisioners/ubuntu/as_root.sh
+++ provisioners/ubuntu/as_root.sh
@@ -1,41 +1,56 @@
 #!/bin/bash
 set -ux
 
-apt update
-apt upgrade
+# only root privilege
+if [ $(whoami) != root ]; then
+  exit
+fi
+
+# apt update -y
+# apt upgrade -y
 
 # Basic
-apt install git
-apt install tig
-apt install tmux
-apt install vim
-apt install ctags
-apt install clang
-apt install silversearcher-ag
-apt install golang # to install peco
+apt install -y git
+apt install -y tig
+apt install -y tmux
+apt install -y vim
+apt install -y ctags
+apt install -y clang
+apt install -y silversearcher-ag
+apt install -y golang
+apt install -y dstat
+
+# Healing
+apt install -y sl
+apt install -y sysvbanner
+apt install -y cowsay
+
+# Web+DB
+apt install -y nginx
+apt install -y jq
 
 # Network
-apt install ssh
+apt install -y ssh
 systemctl enable ssh.service
 systemctl start  ssh.service
 
 # Kernel
-apt install libncurses-dev
-apt install build-essential
-apt install fakeroot
-apt install kernel-package
-apt install linux-source
+apt install -y libncurses-dev
+apt install -y build-essential
+apt install -y fakeroot
+apt install -y kernel-package
+apt install -y linux-source
 
 # Ruby
-apt install autoconf
-apt install bison
-apt install libssl-dev
-apt install libyaml-dev
-apt install libreadline6-dev
-apt install zlib1g-dev
-apt intalll libncurses5-dev
-apt install libffi-dev
-apt install libgdbm3
-apt install libgdbm-dev
+apt install -y autoconf
+apt install -y bison
+apt install -y libssl-dev
+apt install -y libyaml-dev
+apt install -y libreadline6-dev
+apt install -y zlib1g-dev
+apt intalll -y libncurses5-dev
+apt install -y libffi-dev
+apt install -y libgdbm3
+apt install -y libgdbm-dev
 
 source ./provisioners/shared/as_root.sh
