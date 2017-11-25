ad2dd5049f10100fd255db85616efd178127e405
Shingo Kawamura <blp1526@gmail.com>
2017-11-25 21:57:01 +0900

    Add goreleaser

--- provisioners/golang/as_user.sh
+++ provisioners/golang/as_user.sh
@@ -3,6 +3,7 @@ set -x
 
 go get -u github.com/derekparker/delve/cmd/dlv
 go get -u github.com/golang/dep/cmd/dep
+go get -u github.com/goreleaser/goreleaser
 # https://github.com/nsf/gocode/issues/368
 go get -u github.com/nsf/gocode
 gocode close
