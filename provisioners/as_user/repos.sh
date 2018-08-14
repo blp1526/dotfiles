da8a1147d3c0841c0bec1fe22d0d9817b29f873f
Shingo Kawamura <blp1526@gmail.com>
2018-08-14 21:59:36 +0900

    Update -maxdepth from 3 to 4

--- provisioners/as_user/repos.sh
+++ provisioners/as_user/repos.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-# http://vim-jp.org/docs/build_linux.html
-git gopath 'git@github.com:blp1526/vim.git'
+# https://github.com/direnv/direnv#install
+git gopath 'https://github.com/direnv/direnv.git'
 
-# https://github.com/blp1526/tig/blob/master/INSTALL.adoc#build-configuration
-# ./autogen.sh
-# LDLIBS=-lncursesw CPPFLAGS=-DHAVE_NCURSESW_CURSES_H ./configure
-# make
-# sudo make install
-git gopath 'git@github.com:blp1526/tig.git'
-
-# https://github.com/blp1526/direnv#install
-git gopath 'git@github.com:blp1526/direnv.git'
-
-# https://github.com/blp1526/gibo#installation
-git gopath 'git@github.com:blp1526/gibo.git'
+# https://github.com/simonwhitaker/gibo#installation
+git gopath 'https://github.com/simonwhitaker/gibo.git'
 
 # https://github.com/peco/peco#building-peco-yourself
 git gopath 'https://github.com/peco/peco.git'
