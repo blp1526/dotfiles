b1bf785f755b061ab8f5730e820d58fc58e98188
Shingo Kawamura <blp1526@gmail.com>
2019-04-21 12:46:45 +0900

    Replace GOPATH to HOME

--- provisioners/user.sh
+++ provisioners/user.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd $(dirname $0)
-dotfiles_path=${HOME}/src/github.com/blp1526/dotfiles
-
 mkdir -p ${HOME}/bin
 mkdir -p ${HOME}/src
 mkdir -p ${HOME}/.config/git
 mkdir -p ${HOME}/.config/peco
-ln -sf ${dotfiles_path}/.config/peco/config.json ~/.config/peco/config.json
-ln -sf ${dotfiles_path}/bin/git-gopath ~/bin/git-gopath
+
+dotfiles_path=${HOME}/src/github.com/blp1526/dotfiles
+ln -sf ${dotfiles_path}/.config/peco/config.json ${HOME}/.config/peco/config.json
 
 file_names=(
   .bash_profile
@@ -17,19 +15,16 @@ file_names=(
   .vimrc
   .tmux.conf
   .tigrc
-  .gemrc
   .inputrc
-  .rubocop.yml
   .lftprc
-  .ctags
 )
 
 for file_name in "${file_names[@]}"; do
-  ln -sf ${dotfiles_path}/${file_name} ~/
+  ln -sf ${dotfiles_path}/${file_name} ${HOME}/${file_name}
 done
 
 if ! type anyenv >/dev/null 2>&1; then
-  git clone https://github.com/anyenv/anyenv ~/.anyenv
+  git clone https://github.com/anyenv/anyenv ${HOME}/.anyenv
 fi
 
 if ! type rustc >/dev/null 2>&1; then
@@ -38,15 +33,15 @@ fi
 
 if ! type direnv >/dev/null 2>&1; then
   # https://github.com/direnv/direnv#install
-  git gopath 'https://github.com/direnv/direnv.git'
+  git clone https://github.com/direnv/direnv.git ${HOME}/src/github.com/direnv/direnv
 fi
 
 if ! type gibo >/dev/null 2>&1; then
   # https://github.com/simonwhitaker/gibo#installation
-  git gopath 'https://github.com/simonwhitaker/gibo.git'
+  git clone https://github.com/simonwhitaker/gibo.git ${HOME}/src/github.com/simonwhitaker/gibo
 fi
 
 if ! type peco >/dev/null 2>&1; then
   # https://github.com/peco/peco#building-peco-yourself
-  git gopath 'https://github.com/peco/peco.git'
+  git clone https://github.com/peco/peco.git ${HOME}/src/github.com/peco/peco
 fi
