6478c1aa2a41898691198c288cdf90aaf1f3b7fb
Shingo Kawamura <blp1526@gmail.com>
2019-07-25 22:49:03 +0900

    Add rootless docker

--- provisioners/root.sh
+++ provisioners/root.sh
@@ -47,6 +47,7 @@ apt install -y uuid-dev
 apt install -y libpopt-dev
 apt install -y libelf-dev
 apt install -y libseccomp-dev
+apt install -y uidmap
 apt install -y linux-tools-common
 apt install -y linux-tools-$(uname -r)
 
