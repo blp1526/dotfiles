76af560fed661550caf926cac73f31e1a6d982bf
Shingo Kawamura <blp1526@gmail.com>
2020-01-22 08:34:02 +0900

    Add sshpass

--- provisioners/root.sh
+++ provisioners/root.sh
@@ -82,6 +82,7 @@ apt install -y libnss-myhostname
 apt install -y traceroute
 apt install -y whois
 apt install -y autossh
+apt install -y sshpass
 apt install -y proxychains4
 apt install -y tsocks
 
