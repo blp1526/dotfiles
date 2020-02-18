ae0e0f20a847cdd366f05bd1290dce76f30ef5bf
Shingo Kawamura <blp1526@gmail.com>
2020-02-19 07:49:32 +0900

    Add provisioners/root_xrdp.sh

---
+++ provisioners/root_xrdp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eux
+
+if [ "$(whoami)" != "root" ]; then
+  echo "Use sudo"
+  exit 1
+fi
+
+grep "ID=ubuntu" /etc/os-release >/dev/null 2>&1
+if [ "$?" != "0" ]; then
+  echo "Unexpected OS"
+  exit 1
+fi
+
+apt update -y
+apt upgrade -y
+apt install -y xrdp
+
+sed -e 's/^new_cursors=true$/new_cursors=false/g' -i /etc/xrdp/xrdp.ini
+sed -e 's/^FuseMountName=thinclient_drives$/FuseMountName=\.thinclient_drives/g' -i /etc/xrdp/sesman.ini
+cat << __EOS__ > /etc/polkit-1/localauthority/50-local.d/xrdp-color-manager.pkla
+[NetworkManager]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+__EOS__
