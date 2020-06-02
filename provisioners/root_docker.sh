d5dcb2795b1e7086e2941719e63046b927493489
Shingo Kawamura <blp1526@gmail.com>
2020-06-02 23:14:41 +0900

    Add code_name to provisioners/root_docker.sh

--- provisioners/root_docker.sh
+++ provisioners/root_docker.sh
@@ -12,6 +12,14 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 
+code_name=$(lsb_release -cs)
+echo "If you don't want docker for ${code_name}, enter specific code name."
+read -r expected_code_name
+
+if [ "${expected_code_name}" != "" ]; then
+  code_name="${expected_code_name}"
+fi
+
 # docker-ce https://docs.docker.com/install/linux/docker-ce/ubuntu
 apt-get install -y apt-transport-https
 apt-get install -y ca-certificates
@@ -20,7 +28,7 @@ apt-get install -y gnupg-agent
 apt-get install -y software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${code_name} stable"
 apt-get update -y
 
 apt-get install -y docker-ce
