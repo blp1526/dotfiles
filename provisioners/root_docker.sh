830f7e7107bee26d75e0afdd3ebfad5a9f772074
Shingo Kawamura <blp1526@gmail.com>
2021-06-02 20:28:06 +0900

    Use dpkg --print-architecture

--- provisioners/root_docker.sh
+++ provisioners/root_docker.sh
@@ -12,13 +12,8 @@ sudo apt-get install -y lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-target="amd64"
-if [ "$(arch)" == "aarch64" ]; then
-  target="arm64"
-fi
-
 echo \
-  "deb [arch=${target} signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update -y
