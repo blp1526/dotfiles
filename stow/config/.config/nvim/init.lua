a9b2f6c0e3de42ed8e7e978f10c8580d1769850a
Shingo Kawamura <blp1526@gmail.com>
2022-07-04 23:49:58 +0900

    Add nvim-cmp

--- stow/config/.config/nvim/init.lua
+++ stow/config/.config/nvim/init.lua
@@ -1,20 +1,43 @@
+-- general settings {{{
 vim.cmd([[
-" general settings {{{
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
-" }}}
 ]])
-
+-- }}}
+-- plugin settings {{{
 require('plugins')
-require('hop').setup()
-
+-- folke/tokyonight.nvim {{{
 vim.cmd([[
-" plugin settings {{{
-" folke/tokyonight.nvim {{{
 colorscheme tokyonight
-" }}}
-" itchyny/lightline.vim {{{
+]])
+-- }}}
+-- hrsh7th/nvim-cmp {{{
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {},
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+-- }}}
+-- itchyny/lightline.vim {{{
+vim.cmd([[
 let g:lightline = {
       \ 'colorscheme': 'tokyonight',
       \ 'active': {
@@ -25,17 +48,59 @@ let g:lightline = {
       \   'gitbranch': 'FugitiveHead'
       \ },
       \ }
-" }}}
-" nvim-telescope/telescope.nvim {{{
-nnoremap <leader>f :Telescope git_files show_untracked=true<CR>
-" }}}
-" phaazon/hop.nvim {{{
-nnoremap <leader>e :HopPattern<CR>
-" }}}
-" }}}
-
-" vim: foldmethod=marker
-" vim: foldcolumn=0
-" vim: foldlevel=0
 ]])
+-- }}}
+-- neovim/nvim-lspconfig {{{
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+-- }}}
+-- nvim-telescope/telescope.nvim {{{
+vim.cmd([[
+nnoremap <leader>f :Telescope git_files show_untracked=true<CR>
+]])
+-- }}}
+-- phaazon/hop.nvim {{{
+require('hop').setup()
+vim.cmd([[
+nnoremap <leader>e :HopPattern<CR>
+]])
+-- }}}
+-- williamboman/nvim-lsp-installer {{{
+local lsp_installer = require "nvim-lsp-installer"
+local lspconfig = require "lspconfig"
+lsp_installer.setup()
+for _, server in ipairs(lsp_installer.get_installed_servers()) do
+  lspconfig[server.name].setup {
+    on_attach = on_attach,
+  }
+end
+-- }}}
+-- }}}
+
+--" vim: foldmethod=marker
+--" vim: foldcolumn=0
+--" vim: foldlevel=0
