1ec1bf9ae11b6cf325d3a1f22a2e5d493a30122e
Shingo Kawamura <blp1526@gmail.com>
2025-01-25 10:52:20 +0900

    Remove gpg-agent.conf

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 set -eux -o pipefail
 
 mkdir -p ~/bin
-mkdir -p ~/.gnupg
 mkdir -p ~/.config/git
 mkdir -p ~/.config/nvim
-stow --dir=stow --target="${HOME}" --verbose root bin gnupg config
+stow --dir=stow --target="${HOME}" --verbose root bin config
