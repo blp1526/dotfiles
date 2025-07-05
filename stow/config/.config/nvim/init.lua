df8949149a7c596ede4575d45ed39bbbcbe20af3
Shingo Kawamura <blp1526@gmail.com>
2025-07-05 12:59:15 +0900

    Remove nvim-lsp-installer

--- stow/config/.config/nvim/init.lua
+++ stow/config/.config/nvim/init.lua
@@ -1,12 +1,11 @@
--- general settings {{{
+-- general settings
 vim.cmd([[
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
 ]])
--- }}}
 
--- plugin settings {{{
+-- plugin settings
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -28,25 +27,28 @@ require("lazy").setup({
   { 'hrsh7th/cmp-cmdline' },
   { 'hrsh7th/cmp-nvim-lsp' },
   { 'hrsh7th/cmp-path' },
-  { 'hrsh7th/cmp-vsnip' },
   { 'hrsh7th/nvim-cmp' },
-  { 'hrsh7th/vim-vsnip' },
   { 'itchyny/lightline.vim' },
   { 'neovim/nvim-lspconfig' },
   { 'preservim/nerdtree' },
   { 'simeji/winresizer' },
   { 'thinca/vim-qfreplace' },
-  { 'williamboman/nvim-lsp-installer' },
-})
-
--- hrsh7th/nvim-cmp {{{
-local cmp = require('cmp')
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+  {
+    'williamboman/mason.nvim',
+    build = ':MasonUpdate',
+    config = function()
+      require('mason').setup()
     end,
   },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
+  },
+})
+
+-- hrsh7th/nvim-cmp
+local cmp = require('cmp')
+cmp.setup({
   window = {},
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -57,31 +59,75 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' },
   }, {
     { name = 'buffer' },
   })
 })
--- }}}
--- neovim/nvim-lspconfig {{{
+-- neovim/nvim-lspconfig
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
--- }}}
--- williamboman/nvim-lsp-installer {{{
-local lsp_installer = require "nvim-lsp-installer"
-local lspconfig = require "lspconfig"
-lsp_installer.setup()
-for _, server in ipairs(lsp_installer.get_installed_servers()) do
-  lspconfig[server.name].setup {
-    on_attach = on_attach,
-  }
-end
--- }}}
--- }}}
 
---" vim: foldmethod=marker
---" vim: foldcolumn=0
---" vim: foldlevel=0
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local setup_lsp = function()
+  local lspconfig = require('lspconfig')
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+  require('mason-lspconfig').setup()
+
+  require('mason-lspconfig').setup_handlers({
+    function(server_name)
+      lspconfig[server_name].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+    ['lua_ls'] = function()
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              globals = {'vim'},
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+    end,
+  })
+end
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LazyDone',
+  callback = setup_lsp,
+})
--- stow/root/.vimrc
+++ stow/root/.vimrc
@@ -1,14 +1,14 @@
 scriptencoding utf-8
 
-" general settings {{{
-" functions {{{
+" general settings
+" functions
 function! JISX0208SpaceHilight()
   syntax match JISX0208Space "　" display containedin=ALL
   highlight JISX0208Space term=underline ctermbg=LightCyan guibg=darkgray gui=underline
 endfunction
-" }}}
-" map {{{
-" nnoremap {{{
+
+" map
+" nnoremap
 nnoremap <silent><ESC><ESC> :nohlsearch<CR>
 " tab
 nnoremap <silent><C-c> :tabclose<CR>
@@ -16,8 +16,7 @@ nnoremap <silent><C-n> :tabnew<CR>
 nnoremap <silent><C-h> :tabprevious<CR>
 nnoremap <silent><C-l> :tabnext<CR>
 nnoremap <silent><C-s> :tab split<CR>
-" }}}
-" inoremap {{{
+" inoremap
 " insert mode emacs keybindings
 inoremap <silent><C-a> <Home>
 inoremap <silent><C-e> <End>
@@ -27,8 +26,7 @@ inoremap <silent><C-d> <Del>
 inoremap <silent><C-k> <C-o>D
 inoremap <silent><C-p> <Up>
 inoremap <silent><C-n> <Down>
-" }}}
-" cnoremap {{{
+" cnoremap
 " command line mode emacs keybindings
 cnoremap <C-a> <Home>
 cnoremap <C-e> <End>
@@ -36,13 +34,11 @@ cnoremap <C-b> <Left>
 cnoremap <C-f> <Right>
 cnoremap <C-d> <Del>
 cnoremap <C-k> <C-o>D
-" }}}
-" tnoremap {{{
+" tnoremap
 " exit terminal
 tnoremap <ESC> <C-\><C-n>
-" }}}
-" }}}
-" augroup {{{
+
+" augroup
 augroup MultiByteSpace
   autocmd!
   autocmd BufNew,BufRead * call JISX0208SpaceHilight()
@@ -52,13 +48,13 @@ augroup Spell
   autocmd!
   autocmd Filetype gitcommit setlocal spell
 augroup END
-" }}}
-" runtime {{{
+
+" runtime
 " /usr/share/vim/vim74/ftplugin/man.vim
 runtime macros/matchit.vim
 runtime ftplugin/man.vim
-" }}}
-" options {{{
+
+" options
 set grepprg=rg\ --vimgrep\ -i\ --hidden\ --glob=!.git/\ --glob=!vendor/
 set noswapfile
 set nobackup
@@ -93,11 +89,9 @@ set nrformats=alpha
 set tags+=.git/tags
 set showtabline=2
 set laststatus=2
-" }}}
-" }}}
 
-" plugin settings {{{
-" ctrlpvim/ctrlp.vim {{{
+" plugin settings
+" ctrlpvim/ctrlp.vim
 let g:ctrlp_prompt_mappings = {
 \ 'PrtSelectMove("j")':   ['<c-n>'],
 \ 'PrtSelectMove("k")':   ['<c-p>'],
@@ -108,11 +102,11 @@ let g:ctrlp_match_window  = 'bottom,order:btt,min:1,max:100,results:100'
 let g:ctrlp_user_command = ['.git', 'cd %s && git ls-files -co --exclude-standard | grep -v "^vendor/" | uniq']
 let g:ctrlp_show_hidden   = 1
 let g:ctrlp_cmd = 'CtrlPCurWD'
-" }}}
-" folke/tokyonight.nvim {{{
+
+" folke/tokyonight.nvim
 colorscheme tokyonight
-" }}}
-" itchyny/lightline.vim {{{
+
+" itchyny/lightline.vim
 " https://github.com/itchyny/lightline.vim/issues/245#issuecomment-375136013
 let g:lightline = {
 \ 'colorscheme': 'tokyonight',
@@ -132,24 +126,18 @@ let g:lightline = {
 function! LightlineCurrentDirectory(n) abort
   return fnamemodify(getcwd(tabpagewinnr(a:n), a:n), ':t')
 endfunction
-" }}}
-" thinca/vim-qfreplace {{{
+
+" thinca/vim-qfreplace
 augroup QuickFixAfterGrep
   autocmd!
   autocmd QuickFixCmdPost *grep* cwindow
 augroup END
-" }}}
-" preservim/nerdtree {{{
+
+" preservim/nerdtree
 let g:NERDTreeShowHidden = 1
 let g:NERDTreeWinSize = 32
 nnoremap <silent><Leader>n :NERDTreeToggle<CR>
-" }}}
-" }}}
 
 set termguicolors
 filetype plugin indent on
 syntax on
-
-" vim: foldmethod=marker
-" vim: foldcolumn=0
-" vim: foldlevel=0
