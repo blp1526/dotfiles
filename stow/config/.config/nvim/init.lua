409ec8ff93b981217d568e116f8c27044eaa973c
Shingo Kawamura <blp1526@gmail.com>
2025-09-12 16:42:08 +0900

    Add desc

--- stow/config/.config/nvim/init.lua
+++ stow/config/.config/nvim/init.lua
@@ -166,16 +166,10 @@ require("lazy").setup({
       })
 
       -- telescope keymaps
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
-      vim.keymap.set('n', '<leader>fc', builtin.commands, {})
-      vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
-      vim.keymap.set('n', '<leader>fw', builtin.lsp_workspace_symbols, {})
-      vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
-      vim.keymap.set('n', '<leader>fR', builtin.lsp_references, {})
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find_files' })
+      vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live_grep' })
+      vim.keymap.set('n', '<leader>c', builtin.commands, { desc = 'Telescope commands' })
+      vim.keymap.set('n', '<leader>d', builtin.lsp_definitions, { desc = 'Telescope lsp_definitions' })
     end,
   },
   {
